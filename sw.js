5fe3bddf4cd56d446d1d427241fcf06c879f939f
Giliard31 <113076201+Giliard31@users.noreply.github.com>
2026-08-27 20:52:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('fetch', function(event) {
-  // Apenas mantém o aplicativo funcional para PWA
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('delivery-store').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
